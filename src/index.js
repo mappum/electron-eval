109aee1102871503345ebe4d9369123fd26ae635
Matt Bell <mappum@gmail.com>
2016-05-24 15:27:05 -0700

    Enable xvfb by default on linux

--- src/index.js
+++ src/index.js
@@ -22,7 +22,11 @@ class Daemon extends EventEmitter {
     opts = opts || {}
     opts.timeout = typeof opts.timeout === 'number' ? opts.timeout : 10e3
     opts.windowOpts = opts.windowOpts || { show: false }
-    if ((opts.headless || process.env.HEADLESS) && Xvfb != null) {
+    opts.headless = opts.headless || process.env.HEADLESS
+    if (opts.headless == null && process.platform === 'linux') {
+      opts.headless = true
+    }
+    if (opts.headless && Xvfb != null) {
       this.xvfb = new Xvfb(opts.xvfb || {})
       this.xvfb.startSync()
     }
